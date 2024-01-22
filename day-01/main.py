ec86e1fe7736e53bf93e6863af8122ef8902536c
Roberth Rios <tauroroberth00@gmail.com>
2024-01-22 17:24:23 -0500

    Second Commit

---
+++ day-01/main.py
@@ -0,0 +1,13 @@
+# Imprimimos en varias lineas
+print('Hola Mundo!')
+print('Mi nombre es Roberth')
+print('Tengo 23 años de edad')
+
+# Imprimimos en una sola lineas
+print('Hola Mundo!\nMinombre es Roberth\nTengo 23 años de edad')
+
+# Concatenacion de cadenass
+print('Hola' + ' ' + 'Mi nombre es Jason')
+
+# Funcion para entrada de texto
+print('Hola ' + input("Cual es tu nombre: ") + "!")
